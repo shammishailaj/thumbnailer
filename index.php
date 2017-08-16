2d0c7d9f15ca35fe129c6d3a7a9f3763d8f9e4cc
Zbigniew Jasek <jasekz1@gmail.com>
2017-08-16 07:41:58 -0500

    Added support for dynamic mime type; added 200 status header for returned thumbnails

--- index.php
+++ index.php
@@ -3,13 +3,15 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../api/api.php';
 require __DIR__ . '/Thumbnailer.php';
 
+use Directus\Util\ArrayUtils;
+
 try {
     if (! file_exists(__DIR__ . '/../api/config.php') || filesize(__DIR__ . '/../api/config.php') == 0) {
         throw new Exception('Invalid Directus config.');
     }
     
     $app = \Directus\Bootstrap::get('app');
-    
+
     $thumbnailer = new Thumbnailer([
         'thumbnailUrlPath' => $app->request->getPathInfo(),
         'configFilePath' => __DIR__ . '/config.json', 
@@ -20,31 +22,25 @@ try {
         
         // http://image.intervention.io/api/crop
         case 'contain':
-            $imagePath = $thumbnailer->contain();
+            $filePath = $thumbnailer->contain();
             break;
             
         // http://image.intervention.io/api/fit
         case 'crop':
         default:
-            $imagePath = $thumbnailer->crop();
+            $filePath = $thumbnailer->crop();
     }
     
-    header('Content-type: image/jpeg');
-    echo file_get_contents($imagePath);
+    header('HTTP/1.1 200 OK');
+    header('Content-type: ' . image_type_to_mime_type(exif_imagetype($filePath)));
+    echo file_get_contents($filePath);
     exit(0);
 }
 
 // all exceptions are handled by displaying an 'image not found' png
 catch (Exception $e) {
-    dd($e);
-    header('Content-type: image/jpeg');
-    echo file_get_contents(__DIR__ . '/img-not-found.png');
+    $config = json_decode(file_get_contents(__DIR__ . '/config.json'), true);    
+    header('Content-type: ' . image_type_to_mime_type(exif_imagetype(ArrayUtils::get($config, '404imageLocation', './img-not-found.png'))));
+    echo file_get_contents($filePath);
     exit(0);
-}
-
-// dev helper
-function dd($c)
-{
-    var_dump($c);
-    die();
 }
